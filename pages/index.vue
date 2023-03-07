3f0107aff18219dfc47b74469c857e1ec2f9b4d2
adisak.t <adisak.t@clicknext.com>
2023-03-07 11:51:42 +0700

    refactor : tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        Hello World 
+        <h1 class="text-3xl font-bold underline bg-red-700">
+            Hello world!
+        </h1>
     </div>
 </template>
 
@@ -8,7 +10,4 @@
 
 </script>
 
-<style lang="scss">
-
-
-</style>
+<style lang="scss"></style>
