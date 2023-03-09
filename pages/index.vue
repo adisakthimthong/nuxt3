1761abfeb97753bc267a43a0b140227145eb52b8
adisak.t <adisak.t@clicknext.com>
2023-03-09 14:01:52 +0700

    add eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline bg-red-700">
-            Hello world!
-        </h1>
-    </div>
+  <div>
+    <h1 class="text-3xl font-bold underline bg-red-700">Hello world!</h1>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss"></style>
